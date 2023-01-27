b2eebd8df2e9981ed74c9565032c2fd59ba6e4e2
Ryu_woong <jin02014@naver.com>
2023-01-27 20:46:04 +0900

    clock.js
    #5.3
    시분초
    1:2:1 처럼 나오는 것을
    01:02:01처럼 나오게 수정
    
    padStrart(몇글자까지 있어야하는가, "빈공간에 채워 넣을 문자")
    함수의 이해

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,12 @@
 const clock = document.querySelector(".clock");
 
-function sayHelloInConsole() {
-  console.log("Hello");
+function getClock() {
+  const time = new Date();
+  const hours = String(time.getHours()).padStart(2, "0");
+  const minutes = String(time.getMinutes()).padStart(2, "0");
+  const secondes = String(time.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${secondes}`;
 }
 
-setInterval(sayHelloInConsole, 2 * 1000);
+getClock();
+setInterval(getClock, 1 * 1000);
