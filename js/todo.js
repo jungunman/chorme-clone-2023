675873e63d9e01e0fb9fc0082bc433e27e75a6f6
Ryu_woong <jin02014@naver.com>
2023-01-31 21:58:01 +0900

    delete todo
    #7.6
    delete funtion  구현
    문제 발생
    중복되는 문자는 한번에 지우나 화면상에서는 하나만 삭제되는 것으로 나옴, 새로고침할 경우 삭제값과 같은 단어는 동시에 삭제
    다음에 해결

--- js/todo.js
+++ js/todo.js
@@ -2,64 +2,66 @@ const toDoForm = document.querySelector(".todo-form");
 const toDoList = document.querySelector(".todo-list");
 const toDoInputText = toDoForm.querySelector(".todo-form input");
 let arrToDo = [];
-const TO_DO_LIST_KEY = "to-do-list"; 
+const TO_DO_LIST_KEY = "to-do-list";
 
-function saveTodo(){
-    //JSON.stringify 는
-    //어떠한 object나 array등을 string으로 만들어줌
-    //문자열로 저장된 것을 JSON.stringify를 이용하면 배열로 저장할 수 있음
-    localStorage.setItem(TO_DO_LIST_KEY, JSON.stringify(arrToDo));
+function saveTodo() {
+  //JSON.stringify 는
+  //어떠한 object나 array등을 string으로 만들어줌
+  //문자열로 저장된 것을 JSON.stringify를 이용하면 배열로 저장할 수 있음
+  localStorage.setItem(TO_DO_LIST_KEY, JSON.stringify(arrToDo));
 }
 
+function deleteTodo(event) {
+  //해당 TodoList 삭제하기
+  const deleteLi = event.target.parentElement;
+  deleteLi.remove();
+  const deleteTarget = JSON.parse(localStorage.getItem(TO_DO_LIST_KEY));
 
-function deleteTodo(event){
-    //해당 TodoList 삭제하기
-    const deleteLi = event.target.parentElement;
-    deleteLi.remove();
-    console.log(event.target.previousSibling.innerText);
-    const deleteTarget = JSON.parse(localStorage.getItem(TO_DO_LIST_KEY));
-    deleteTarget.filter(event.target.previousSibling.innerText);
-    localStorage.setItem(TO_DO_LIST_KEY,JSON.stringify(deleteTarget));
-    //버튼만 사라지는 것을 볼 수 있다. li 전체를 삭제하려면!? 위의 코드와 같이
-    // const deleteLi = event.target;
+  //해당 타겟 삭제
+  let filteredTarget = deleteTarget.filter(
+    (element) => element !== event.target.previousSibling.innerText
+  );
+  arrToDo = filteredTarget;
+  localStorage.setItem(TO_DO_LIST_KEY, JSON.stringify(filteredTarget));
+  //버튼만 사라지는 것을 볼 수 있다. li 전체를 삭제하려면!? 위의 코드와 같이
+  // const deleteLi = event.target;
 }
 
-function paintTodo(newToDo){
-    const li = document.createElement("li");
-    const span = document.createElement("span");
-    span.innerText = newToDo;
-    const button = document.createElement("button");
-    button.innerText = "X";
-    //삭제 이벤트
-    button.addEventListener("click",deleteTodo);
-    
-    //
-    li.appendChild(span);
-    li.appendChild(button);
-    toDoList.appendChild(li);
+function paintTodo(newToDo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerText = newToDo;
+  const button = document.createElement("button");
+  button.innerText = "X";
+  //삭제 이벤트
+  button.addEventListener("click", deleteTodo);
+
+  //
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
 }
 
-function handleToDoSubmit(event){
-    event.preventDefault();
-    const newToDo = toDoInputText.value;
-    //input 창 값 초기화
-    toDoInputText.value=""
-    arrToDo.push(newToDo);
-    paintTodo(newToDo);
-    saveTodo();
-    //localStorage는 배열도, String으로 저장해서 여러개 값을 저장할 수 없음 -> 또한 덮어쓰기 형식임
-    // localStorage.setItem(TO_DO_LIST_KEY,arrToDo);
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newToDo = toDoInputText.value;
+  //input 창 값 초기화
+  toDoInputText.value = "";
+  arrToDo.push(newToDo);
+  paintTodo(newToDo);
+  saveTodo();
+  //localStorage는 배열도, String으로 저장해서 여러개 값을 저장할 수 없음 -> 또한 덮어쓰기 형식임
+  // localStorage.setItem(TO_DO_LIST_KEY,arrToDo);
 }
 
-
-toDoForm.addEventListener("submit",handleToDoSubmit);
+toDoForm.addEventListener("submit", handleToDoSubmit);
 
 const savedToDos = localStorage.getItem(TO_DO_LIST_KEY);
 
 //저장된 todolist가 있으면 출력
 //forEach의 처음 사용
-if(savedToDos !== null){
-    const parsedToDos = JSON.parse(savedToDos);
-    arrToDo = parsedToDos;
-    parsedToDos.forEach(paintTodo);
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  arrToDo = parsedToDos;
+  parsedToDos.forEach(paintTodo);
 }
